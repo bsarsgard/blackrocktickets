dba034dab6197521bd4fbf0f84cc55205df5e9be
bsarsgard <bsarsgard@atzok.com>
2015-04-12 23:47:24 +0000

    admin updates

---
+++ txqueue/admin.py
@@ -0,0 +1,23 @@
+"""
+    Texas - Ticket Sales System
+    Copyright (C) 2010 Ben Sarsgard
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+from tickets.txqueue.models import *
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django import forms
+
+admin.site.register(QueuedTier)
